682d05ae7d06e0fc7b02e674c583695e0a6e905d
kuwalab <arata.shinichi@gmail.com>
2014-07-21 07:35:08 +0900

    最初のコミット

---
+++ src/main/java/com/example/spring/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		return "hello/index";
+	}
+}
