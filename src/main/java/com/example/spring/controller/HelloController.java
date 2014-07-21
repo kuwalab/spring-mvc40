fe381c4419301fe3433c2294801668a277cea678
kuwalab <arata.shinichi@gmail.com>
2014-07-21 17:26:36 +0900

    URLのパラメータを受け取るサンプルの追加

--- src/main/java/com/example/spring/controller/HelloController.java
+++ src/main/java/com/example/spring/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,5 +11,17 @@ public class HelloController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 		return "hello/index";
+	}
+
+	@RequestMapping(value = "/urlparam/{var}", method = RequestMethod.GET)
+	public String urlparam(@PathVariable String var, Model model) {
+		model.addAttribute("var", var);
+		return "hello/urlparam";
+	}
+
+	@RequestMapping(value = "/urlparam2/{var}", method = RequestMethod.GET)
+	public String urlparam2(@PathVariable("var") String var1, Model model) {
+		model.addAttribute("var", var1);
+		return "hello/urlparam";
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/hello/urlparam.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html; charset=utf-8" %><%--
+--%><!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>サンプル</title>
+ </head>
+ <body>
+パラメータは: <c:out value="${var}" />
+ </body>
+</html>
