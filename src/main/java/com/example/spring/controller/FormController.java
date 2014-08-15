ec768eb40e84adbac3030c57ef92dd6eca9b48aa
kuwalab <arata.shinichi@gmail.com>
2014-08-15 17:01:03 +0900

    033、余計なブランクの除去

--- src/main/java/com/example/spring/controller/FormController.java
+++ src/main/java/com/example/spring/controller/FormController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FormController {
 	@RequestMapping("/form/input")
 	public String input(Model model) {
-		Book book = new Book("よく分かるSpring", 2000, 2500);
+		Book book = new Book("よく分かるSpring<&>", 2000, 2500);
 		model.addAttribute("book", book);
 		return "form/input";
 	}
-
 }
