77afa7d0892c737d471194e80b98b8fdabeaef69
kuwalab <arata.shinichi@gmail.com>
2014-08-24 14:02:38 +0900

    001のテストケースの追加

---
+++ src/test/java/com/example/spring/controller/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.spring.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/spring-context.xml" })
+public class HelloControllerTest {
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = webAppContextSetup(wac).build();
+	}
+
+	@Test
+	public void indexへのGET() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk())
+				.andExpect(view().name("hello/index"))
+				.andExpect(model().hasNoErrors());
+	}
+}
