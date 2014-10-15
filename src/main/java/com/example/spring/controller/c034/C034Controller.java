c6c1c07e1ba16a383ceb6614c2c026111c46f729
kuwalab <arata.shinichi@gmail.com>
2014-10-15 20:05:36 +0900

    034

--- src/main/java/com/example/spring/controller/c034/C034Controller.java
+++ src/main/java/com/example/spring/controller/c034/C034Controller.java
@@ -1,6 +1,7 @@
 package com.example.spring.controller.c034;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,13 @@ public class C034Controller {
 	}
 
 	@RequestMapping(value = "/arrayRecv", method = RequestMethod.POST)
-	public String entityRecv(@RequestParam String[] name) {
-		return "c034/arrayForm";
+	public String entityRecv(@RequestParam String[] name, Model model) {
+		model.addAttribute("name0", "");
+		model.addAttribute("name1", "");
+		model.addAttribute("name2", "");
+		for (int i = 0; i < name.length; i++) {
+			model.addAttribute("name" + i, name[i]);
+		}
+		return "c034/arrayRecv";
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/c034/arrayRecv.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html; charset=utf-8" %><%--
+--%><!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>サンプル</title>
+ </head>
+ <body>
+  name0: ${name0}<br>
+  name1: ${name1}<br>
+  name2: ${name2}
+ </body>
+</html>
